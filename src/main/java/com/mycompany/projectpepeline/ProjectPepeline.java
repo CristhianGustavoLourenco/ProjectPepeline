314015be0b4a8669accbb563f03c031136ec3719
CristhianGustavoLourenco <cristhianlourenco90@gmail.com>
2025-06-04 00:15:01 -0300

    Postagem do Projeto ProjectPepeline

---
+++ src/main/java/com/mycompany/projectpepeline/ProjectPepeline.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectpepeline;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author crist
+ */
+public class ProjectPepeline {
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(3,4,5,10,7);
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
+        System.out.println(Arrays.toString(st1.toArray()));
+        
+        int sum = list.stream().reduce(0, (x,y) -> x + y);
+        System.out.println("Sum = " + sum);
+        
+        List<Integer> newList = list.stream()
+                .filter(x -> x % 2 == 0)
+                .map(x -> x * 10)
+                .collect(Collectors.toList());
+        
+        System.out.println(Arrays.toString(newList.toArray()));
+        
+    }
+}
